358369f471474ee7ccd2231221811b8ac9d79846
Vinayak-Pai <vinnu1pai@gmail.com>
2017-05-21 14:27:51 +0530

    Add files via upload
    Sftp files

---
+++ SSH_connect.py
@@ -0,0 +1,30 @@
+import paramiko
+
+__author__ = 'vinayak'
+
+hostname, username, password = '', ' ', ' '
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+try:
+    ssh.connect(hostname=hostname, username=username, password=password)
+    print "connected successfully!"
+except Exception,e:
+    print "Connection error",e
+
+'''
+sftp.get(remotepath,localpath)
+sftp.put(localpath,remotepath)
+'''
+
+sftp = ssh.open_sftp()
+sftp.put(" " ,
+         " ")
+
+sftp.close()
+print "copied successfully!"
+
+ssh.close()
+
+
+
