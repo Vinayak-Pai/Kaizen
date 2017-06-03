a41b329f617ea9217f54e76f3b7dc84178f42fef
Vinayak-pai <vinnu1pai@gmail.com>
2017-06-03 16:48:22 +0530

    "file to get meaning of verb words"

---
+++ get_meaning.py
@@ -0,0 +1,14 @@
+from PyDictionary import PyDictionary
+
+__author__ = 'vinayak'
+
+input_word = raw_input("Enter the word:")
+
+meaning = PyDictionary().meaning(input_word)
+
+if meaning and meaning['Noun']:
+    print "Meaning of the word \"{}\":".format(input_word)
+    for i in meaning['Noun']:
+        print "* {}".format(i)
+else:
+    print " Sorry, word not found in our database"
