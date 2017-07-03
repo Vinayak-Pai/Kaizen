dc4ddc36f929ec64a99496cccba63eaa90e55898
Vinayak-Pai <vinnu1pai@gmail.com>
2017-07-03 16:24:37 +0530

    Update count_ones.py
    correcting the errors

--- count_ones.py
+++ count_ones.py
@@ -2,14 +2,25 @@ __author__ = "vinayak"
 
 
 def executor_ones(bin_num):
-    output = check_longest_ones (bin_num)
-    for i in range(len(bin_num)):
-        if bin_num[i] == '0':
-            greater_len = swap_and_check(bin_num, i, output)
-            if greater_len > output:
-                output = greater_len
-    print "The greatest length of number of ones in the given input {} is {}".format(bin_num,output)
+    valid = check_for_validity(bin_num)
+    if valid:
+        output = check_longest_ones (bin_num)
+        for i in range(len(bin_num)):
+            if bin_num[i] == '0':
+                greater_len = swap_and_check(bin_num, i, output)
+                if greater_len > output:
+                    output = greater_len
+        print "The greatest length of number of ones in the given input {} is {}".format(bin_num,output)
 
+def check_for_validity(bin_num):
+    bin = ['0', '1']
+    for i in bin_num:
+        if i in bin:
+            pass
+        else:
+            print " Invalid binary number, please enter 0's and 1's"
+            return False
+    return True
 
 def swap_and_check(bin_num, index, output):
     for j in range(len(bin_num)):
@@ -24,17 +35,21 @@ def swap_and_check(bin_num, index, output):
 def check_longest_ones(bin_num):
     cnt_ones = 0
     ones =0
-    for k in range(len(bin_num)):
-        if bin_num[k] == '1':
-            cnt_ones = cnt_ones + 1
-        elif bin_num[k] == '0':
-            if cnt_ones > ones:
-                ones = cnt_ones
-            cnt_ones = 0
+    if '0' not in bin_num:
+       ones = len(bin_num)
+    else:
+        for k in range(len(bin_num)):
+            if bin_num[k] == '1':
+                cnt_ones = cnt_ones + 1
+                if cnt_ones > ones:
+                    ones = cnt_ones
+            else:
+                cnt_ones = 0
     return ones
 
 
 if __name__ == "__main__":
     binary_input = raw_input(" Enter the binary number: ")
     executor_ones(binary_input)
+
 
